a2ac050419700eebe9503f50acc5414dfe1ce0cb
Blockchain <liumingxing@sunmi.com>
2019-08-12 10:36:47 +0800

    magical bayes

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"magicalbayes/bayes/brain"
+	"magicalbayes/bayes/classifier"
+)
+
+func main()  {
+ 	test0()
+
+}
+
+func test0()  {
+	bayesBrain := brain.NewBayesBrain()
+	bayesClassifier := classifier.BayesClassifier{Brain: bayesBrain}
+	bayesBrain.Learn("Chinese", "Chinese", "Beijing", "Chinese")
+	bayesBrain.Learn("Chinese", "Chinese", "Chinese", "Shanghai")
+	bayesBrain.Learn("Chinese", "Chinese", "Macao")
+	bayesBrain.Learn("Not Chinese", "Tokyo", "Japan", "Chinese")
+	bayesBrain.Show()
+
+	features := []string{"Chinese", "Chinese", "Chinese", "Tokyo", "Japan"}
+
+	fmt.Println(bayesClassifier.BayesProbabilityOf("Chinese",
+		features...))
+	fmt.Println(bayesClassifier.BayesProbabilityOf("Not Chinese",
+		features...))
+
+	fmt.Println(bayesClassifier.Classify(features...))
+
+
+}
